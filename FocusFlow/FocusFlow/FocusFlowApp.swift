a45ef741d73eff525ce8b3244027a379381233fa
Sofiia Poteichuk <psa@Sofiias-iMac.local>
2025-12-12 22:47:55 +0200

    init

---
+++ FocusFlow/FocusFlow/FocusFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusFlowApp.swift
+//  FocusFlow
+//
+//  Created by Sofiia Poteichuk on 12.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct FocusFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
